5d98eac493d72c24e788c8c4b9a8a86d35e0a311
yureto <yut465p@gamil.com>
2023-09-01 20:29:01 +0900

    helloworldをヤッホーに変更。gitignoreにideaを追加しました。

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         // hello worldを10回表示する
         for (int i = 0; i < 10; i++)
-            System.out.println("Hello world!");
+            System.out.println("ヤッホー!");
     }
 }
